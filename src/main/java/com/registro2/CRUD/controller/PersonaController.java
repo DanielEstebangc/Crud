3d059e8f68b3e3799b0b65795d4455dc5fc5a06a
danie <danie@Daniel>
2024-12-25 16:22:09 -0500

    modificación en application.properties para que pueda ver mas informacion en los logs de la consola. implementación del frontend. corrección de errores en la lógica de la aplicación El objeto persona en el formulario no estaba correctamente referenciado porque faltaba el signo $. El campo oculto para id estaba usando th:field="{id} en lugar de th:field="*{id}. La expresión condicional en el título usaba una sintaxis incorrecta para Thymeleaf. El namespace estaba mal escrito como http://www.thymeleaft.org, cuando debe ser http://www.thymeleaf.org. ya se puede registrar nuevas personas

--- src/main/java/com/registro2/CRUD/controller/PersonaController.java
+++ src/main/java/com/registro2/CRUD/controller/PersonaController.java
@@ -7,12 +7,14 @@ package com.registro2.CRUD.controller;
 import com.registro2.CRUD.model.Persona;
 import com.registro2.CRUD.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -27,49 +29,37 @@ public class PersonaController {
 
     @GetMapping
     public String listarPersonas(Model model) {
-        model.addAttribute("persona", personaService.listarTodas());
+        model.addAttribute("personas", personaService.listarTodas());
         return "persona-list";
     }
 
     @GetMapping("/nuevo")
-    public String mostrarFormularioNuevaPersona(Model model){
+    public String mostrarFormularioNuevaPersona(Model model) {
         model.addAttribute("persona", new Persona());
         return "persona-form";
     }
-    
+
     @PostMapping
-    public String guardarPersona(Persona persona){
+    public String guardarPersona(Persona persona) {
         personaService.guardar(persona);
         return "redirect:/personas";
-        
+
     }
-    
+
     @GetMapping("/editar/{id}")
-    public String mostrarFormularioEditarPersona(@PathVariable Long id,Model model){
-        model.addAttribute("persona",personaService.obtenerPorId(id));
+    public String mostrarFormularioEditarPersona(@PathVariable Long id, Model model) {
+        Persona persona = personaService.obtenerPorId(id);
+        if (persona == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Persona no encontrada");
+        }
+        model.addAttribute("persona", persona);
         return "persona-form";
     }
-    
+
     @GetMapping("/eliminar/{id}")
-    public String eliminarPersona(@PathVariable Long id){
+    public String eliminarPersona(@PathVariable Long id) {
         personaService.eliminar(id);
         return "redirect:/personas";
     }
-    
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-
-    
-    
 
 }
