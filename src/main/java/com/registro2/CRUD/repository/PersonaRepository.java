7c162e328577601883a4813f47cc4f80904d769c
danie <danie@Daniel>
2024-12-25 16:26:16 -0500

    modificación en application.properties para que pueda ver mas informacion en los logs de la consola. implementación del frontend. corrección de errores en la lógica de la aplicación El objeto persona en el formulario no estaba correctamente referenciado porque faltaba el signo $. El campo oculto para id estaba usando th:field="{id} en lugar de th:field="*{id}. La expresión condicional en el título usaba una sintaxis incorrecta para Thymeleaf. El namespace estaba mal escrito como http://www.thymeleaft.org, cuando debe ser http://www.thymeleaf.org. ya se puede registrar nuevas personas

---
+++ src/main/java/com/registro2/CRUD/repository/PersonaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.registro2.CRUD.repository;
+
+import com.registro2.CRUD.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author danie
+ */
+public interface PersonaRepository extends JpaRepository <Persona, Long>{
+    
+    
+    
+    
+    
+    
+}
